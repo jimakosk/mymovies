f0f7d34afe9486bb14ba7af5d0f08befed4ac2b6
dimitris kontos <jkon258@gmail.com>
2020-03-12 11:11:22 +0200

    m

---
+++ movie/selectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  selectViewController.swift
+//  movie
+//
+//  Created by dimitris kontos on 11/3/20.
+//  Copyright © 2020 dimitris kontos. All rights reserved.
+//
+
+import UIKit
+
+class selectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
